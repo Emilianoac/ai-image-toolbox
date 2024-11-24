5a626a8c9e873c64fda7b609f84f0178924e369b
Emiliano <47641194+Emilianoac@users.noreply.github.com>
2024-11-24 16:16:31 -0300

    FEAT: Add metadata for layout and background remover sections

--- src/app/dashboard/(sections)/remove-background/page.tsx
+++ src/app/dashboard/(sections)/remove-background/page.tsx
@@ -1,5 +1,13 @@
 import BackgroundRemoverClient from "@/components/dashboard/background-remover/BackgroundRemoverClient";
 
+import type { Metadata } from "next";
+import appMetaData from "@/constants/meta-data";
+
+export const metadata: Metadata = {
+  title: appMetaData.removeBackground.title,
+  description: appMetaData.removeBackground.description
+}
+
 export default function page() {
 
   return (
---
+++ src/constants/meta-data.ts
@@ -0,0 +1,13 @@
+
+const appMetaData = {
+  default: {
+    title: "AI Image Toolbox",
+    description: `Descubre AI Image Toolbox, la aplicación web con herramientas de inteligencia artificial para transformar y mejorar tus imágenes. ¡Prueba ahora y lleva tus fotos al siguiente nivel`,
+  },
+  removeBackground: {
+    title: "Eliminar Fondo de Imágenes",
+    description: `Elimina el fondo de tus imágenes en segundos. ¡Prueba ahora y lleva tus fotos al siguiente nivel!`,
+  }
+}
+
+export default appMetaData;
