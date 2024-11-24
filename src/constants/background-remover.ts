f0daf7fd55481bc1295f595a3b982a6d63920a67
Emiliano <47641194+Emilianoac@users.noreply.github.com>
2024-11-24 14:36:39 -0300

    FEAT: Add examples images to constants file.

---
+++ src/constants/background-remover.ts
@@ -0,0 +1,29 @@
+
+const examplesImages = [
+  {
+    title: "Hombre en la naturaleza",
+    src: "/images/examples/background-remover/man.webp",
+  },
+  {
+    title: "Maneki Neko",
+    src: "/images/examples/background-remover/maneki-neko.webp",
+  },
+  {
+    title: "Audifonos",
+    src: "/images/examples/background-remover/headphones.webp",
+  },
+  {
+    title: "Gato bajo la lluvia",
+    src: "/images/examples/background-remover/cat-rain.webp",
+  },
+  {
+    title: "Cerdo feliz",
+    src: "/images/examples/background-remover/pig.webp",
+  },
+  {
+    title: "Gato sentado",
+    src: "/images/examples/background-remover/cat.webp",
+  }
+]
+
+export { examplesImages }
