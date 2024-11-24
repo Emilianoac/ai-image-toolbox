0f6ed4570d8a50e1b0b7a9f9ddb9c8b8ef349656
Emiliano <47641194+Emilianoac@users.noreply.github.com>
2024-11-24 14:16:24 -0300

    FIX: Adjust Section padding in small devices.

--- src/app/dashboard/(sections)/layout.tsx
+++ src/app/dashboard/(sections)/layout.tsx
@@ -10,12 +10,10 @@ export default function DashboardLayout({  children }: Readonly<DashboardLayoutP
   return (
     <div className="grid grid-cols-[_50px_1fr] h-[calc(100vh-50px)]">
       <Sidebar/>
-      <main className="flex flex-col items-center justify-center relative overflow-hidden">
+      <main className="flex flex-col items-center justify-center relative overflow-hidden p-3 md:p-10">
         <GridPattern className="absolute top-0"/>
         <div className="relative z-10 w-full h-full flex justify-center items-center">
-          <div>
-            {children}
-          </div>
+          {children}
         </div>
       </main>
     </div>
