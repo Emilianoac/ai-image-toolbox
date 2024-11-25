c5128fc6698340b874883dd4826bc669cacce37d
Emiliano <47641194+Emilianoac@users.noreply.github.com>
2024-11-25 01:57:56 -0300

    STYLE: Change Theme toggle icons.

--- src/components/globals/AppThemeToggle.tsx
+++ src/components/globals/AppThemeToggle.tsx
@@ -2,7 +2,7 @@
 
 import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
-
+import { FaMoon, FaSun} from "react-icons/fa";
 
 export default function AppThemeToggle() {
   const { setTheme, theme} = useTheme();
@@ -24,10 +24,11 @@ export default function AppThemeToggle() {
       className="
       rounded-full flex items-center justify-center
       transition-colors duration-200 ease-in-out
-      bg-slate-200 dark:bg-slate-800
-      hover:bg-slate-300 dark:hover:bg-blue-gray
+      bg-slate-200 
+      dark:bg-app-dark-100
+      hover:bg-slate-300 dark:hover:bg-app-dark-200
       h-[30px] w-[30px]">
-      {themeReady && theme === "dark" ? "🌞" : "🌙"}
+      {themeReady && theme === "dark" ? <FaSun/> : <FaMoon />}
     </button>
   )
 }
