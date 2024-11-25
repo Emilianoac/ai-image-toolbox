87f9ffe0f093d857de6243f15aa501ae87524a7b
Emiliano <47641194+Emilianoac@users.noreply.github.com>
2024-11-25 03:37:08 -0300

    STYLE: Add margin bottom to main content area in small devices

--- src/app/dashboard/(sections)/layout.tsx
+++ src/app/dashboard/(sections)/layout.tsx
@@ -10,7 +10,7 @@ export default function DashboardLayout({  children }: Readonly<DashboardLayoutP
   return (
     <div className="block lg:grid grid-cols-[_50px_1fr] h-[calc(100vh-50px)]">
       <Sidebar/>
-      <main className="flex flex-col items-center justify-center relative overflow-hidden p-3 md:p-10">
+      <main className="flex flex-col items-center justify-center relative overflow-hidden p-3 md:p-10 mb-20 lg:mb-0">
         <GridPattern className="absolute top-0"/>
         <div className="relative z-10 w-full h-full flex justify-center items-center">
           {children}
