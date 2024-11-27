1c0116ee3f8dcaf72e605e77f2f019f5c3cc3663
Emiliano <47641194+Emilianoac@users.noreply.github.com>
2024-11-27 09:48:43 -0300

    FEAT: Add optional background control to AppLoader component

--- src/components/globals/AppLoader/AppLoader.tsx
+++ src/components/globals/AppLoader/AppLoader.tsx
@@ -2,11 +2,12 @@ import Styles from "./AppLoader.module.css";
 
 interface AppLoaderProps {
   text?: string;
+  includeBackground?: boolean;
 }
 
-export default function AppLoader({text}: AppLoaderProps) {
+export default function AppLoader({text, includeBackground = true}: AppLoaderProps) {
   return (
-  <div className={`${Styles['app-loader-loader']}`}>
+  <div className={`${Styles['app-loader-loader']} ${!includeBackground && "!bg-transparent"}`}>
     <div className={`${Styles['app-loader-loader__spinner']}`}></div>
     <p className={`${Styles['app-loader-loader__text']}`}>
       {text || "Cargando..."}
