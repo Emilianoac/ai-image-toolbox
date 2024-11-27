4343139a43ae15afc392fb833a121ead29c916d6
Emiliano <47641194+Emilianoac@users.noreply.github.com>
2024-11-26 23:14:33 -0300

    FEAT: Add 3D examples and lighting constants

---
+++ src/constants/image-to-3d.ts
@@ -0,0 +1,44 @@
+
+const examples = [
+  {
+    name: "Gato Samurai",
+    image: "/3d/examples/samurai-cat/samurai-cat.webp",
+    glb: "/3d/examples/samurai-cat/samurai-cat.glb",
+  },
+  {
+    name: "Monstruo Rosa",
+    image: "/3d/examples/pink-monster/pink-monster.webp",
+    glb: "/3d/examples/pink-monster/pink-monster.glb",
+  },
+  {
+    name: "Maneki Neko",
+    image: "/3d/examples/maneki-neko/maneki-neko.webp",
+    glb: "/3d/examples/maneki-neko/maneki-neko.glb",
+  },
+  {
+    name: "Monstruo Verde",
+    image: "/3d/examples/green-monster/green-monster.webp",
+    glb: "/3d/examples/green-monster/green-monster.glb",
+  }
+]
+
+const lights = [
+  {
+    name: "Neon Photostudio",
+    file: "/3d/lights/neon_photostudio_1k.hdr",
+  },
+  {
+    name: "Metro noord",
+    file: "/3d/lights/metro_noord_1k.hdr",
+  },
+  {
+    name: "Peppermint powerplant",
+    file: "/3d/lights/peppermint_powerplant_1k.hdr",
+  },
+  {
+    name: "Rainforest trail",
+    file: "/3d/lights/rainforest_trail_1k.hdr",
+  }
+]
+
+export { examples, lights };
