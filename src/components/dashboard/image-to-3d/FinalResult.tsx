23bdf90bc5a5971410e8a6fb3121d0b5df9da52c
Emiliano <47641194+Emilianoac@users.noreply.github.com>
2024-11-27 23:24:47 -0300

    FEAT: Refactor layout and styles in FinalResult component for improved responsiveness

--- src/components/dashboard/image-to-3d/FinalResult.tsx
+++ src/components/dashboard/image-to-3d/FinalResult.tsx
@@ -20,8 +20,8 @@ export default function Result3D({ result, originalImage, reset }: Result3DProps
 
   return (
     <>
-      <div className="grid grid-cols-[700px_1fr] gap-8 p-4 mx-auto max-w-full">
-        <div className="grid grid-cols-2 gap-4">
+      <div className="block lg:grid grid-cols-[700px_1fr] gap-8 p-4 mx-auto max-w-full">
+        <div className="grid grid-cols-[repeat(auto-fit,minmax(300px,1fr))] gap-4">
           {/* Original */}
           <div>
             <p className="text-sm font-bold mb-2">Original</p>
@@ -30,13 +30,13 @@ export default function Result3D({ result, originalImage, reset }: Result3DProps
               width={350}
               height={350}
               alt="Imagen Original"
-              className="w-[350px] h-[350px] mx-auto rounded-md max-w-full object-cover"
+              className="w-[290px] h-auto lg:w-[350px] lg:h-[350px] mx-auto rounded-md max-w-full object-cover"
             />
           </div>
           
           {/* Modelo 3D */}
           <div className="flex items-start gap-2">
-            <div>
+            <div className="w-full">
               <p className="text-sm font-bold mb-2">Modelo 3D</p>
               <DynamicModelViewer
                 src={result}
@@ -44,7 +44,7 @@ export default function Result3D({ result, originalImage, reset }: Result3DProps
                 environment-image={lights[currentLight].file}
                 camera-orbit="180deg 75deg 6m"
                 camera-controls
-                className="w-[350px] h-[350px] mx-auto rounded-md max-full"
+                className="w-[290px] h-[290px] lg:w-[350px] lg:h-[350px] mx-auto rounded-md max-full"
                 style={{
                   background: "radial-gradient(rgb(76, 71, 71), #212121, rgb(0, 0, 0))",
                 }}
@@ -54,8 +54,8 @@ export default function Result3D({ result, originalImage, reset }: Result3DProps
         </div>
 
         {/* Acciones */}
-        <div>
-          <ul className="flex flex-col gap-2">
+        <div className="mt-4 lg-mt-0">
+          <ul className="flex flex-row lg:flex-col gap-2">
             <li>
               <button
                 title="Generar otro modelo"
